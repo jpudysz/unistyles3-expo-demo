0dfe9ba481d6180e6296af96a8d6092dc5957d7f
Jacek Pudysz <jacekpudysz@gmail.com>
2025-03-25 13:02:21 +0100

    feat: boilerplate

---
+++ app/(home)/_layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+
+export default function HomeLayout() {
+    return (
+        <Stack
+            screenOptions={{ headerShown: false }}
+            initialRouteName="index"
+        >
+            <Stack.Screen name="index" />
+            <Stack.Screen name="hotel" />
+        </Stack>
+    )
+}
